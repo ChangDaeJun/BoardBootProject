36d5c6bf0280661ade295c6a72899f1289dbb4b7
ChangDaeJun <eowns1209@gmail.com>
2023-03-22 17:11:16 +0900

    fix : 유저 삭제 시 트랜젝션 처리하도록 조치

--- src/main/java/com/boot/boardbootproject/user/serviceImpl/UserDeleteServiceImpl.java
+++ src/main/java/com/boot/boardbootproject/user/serviceImpl/UserDeleteServiceImpl.java
@@ -7,6 +7,7 @@ import com.boot.boardbootproject.user.repository.UserRepository;
 import com.boot.boardbootproject.user.service.UserDeleteService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -15,6 +16,7 @@ public class UserDeleteServiceImpl implements UserDeleteService {
     private final BoardDeleteService boardDeleteService;
     private final LikeCancelService likeCancelService;
     @Override
+    @Transactional
     public void delete(Long userId) throws Exception{
         User user = userRepository.findById(userId).get();
         boardDeleteService.deleteByUserId(userId);
