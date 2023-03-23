5c2c1d1764c311b1295ae13c12e73a45af5efb6e
ChangDaeJun <eowns1209@gmail.com>
2023-03-24 00:56:38 +0900

    ver 1.0

--- src/main/java/com/boot/boardbootproject/controller/UserController.java
+++ src/main/java/com/boot/boardbootproject/controller/UserController.java
@@ -3,12 +3,15 @@ package com.boot.boardbootproject.controller;
 import com.boot.boardbootproject.user.dto.UserGetForm;
 import com.boot.boardbootproject.user.dto.UserJoinForm;
 import com.boot.boardbootproject.user.dto.UserLoginForm;
+import com.boot.boardbootproject.user.service.UserDeleteService;
 import com.boot.boardbootproject.user.service.UserGetService;
 import com.boot.boardbootproject.user.service.UserJoinService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -16,10 +19,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
     private final UserJoinService userJoinService;
     private final UserGetService userGetService;
+    private final UserDeleteService userDeleteService;
 
     @PostMapping("/user/join")
     public String join(UserJoinForm form) throws Exception{
         userJoinService.join(form);
+        return "index";
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String delete(@PathVariable Long id) throws Exception{
+        userDeleteService.delete(id);
         return "index";
     }
 
